8ae44acd760d97d5b4f90823fd4468eff198bace
231052 <231052@CPD02-M17>
2025-03-13 15:47:07 -0300

    Adicionar arquivos de projeto.

---
+++ TCCapp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace TCCapp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnEmbarcarClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                Embarcar.Text = $"Clicked {count} time";
+            else
+                Embarcar.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(Embarcar.Text);
+        }
+
+        private void nome_Focused(object sender, FocusEventArgs e)
+        {
+
+        }
+    }
+
+}
